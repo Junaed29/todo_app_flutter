7601756541e8d8b8f100a7bd47bec0f18e94cf45
Junaed <junaed1995@gmail.com>
2021-12-10 02:15:44 +0600

    TodoListItem changed to StatefulWidget, Checkbox widget separated as TodoCheckBox

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
 
-class TodoListItem extends StatelessWidget {
+class TodoListItem extends StatefulWidget {
+  @override
+  State<TodoListItem> createState() => _TodoListItemState();
+}
+
+class _TodoListItemState extends State<TodoListItem> {
+  bool isChacked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const Text(
+      leading: Text(
         "Buy milk",
         style: TextStyle(
+          decoration:
+              isChacked ? TextDecoration.lineThrough : TextDecoration.none,
           color: Colors.black,
           fontSize: 20,
         ),
       ),
-      trailing: Checkbox(
-        value: false,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        side: const BorderSide(color: Colors.black, width: 2.5),
-        onChanged: (value) {},
+      trailing: TodoCheckBox(
+        isChanged: isChacked,
+        isChangedCallback: (isChanged) {
+          setState(() {
+            isChacked = isChanged!;
+          });
+        },
       ),
+    );
+  }
+}
+
+class TodoCheckBox extends StatelessWidget {
+  bool isChanged = false;
+  Function(bool?)? isChangedCallback;
+
+  TodoCheckBox({required this.isChanged, required this.isChangedCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChanged,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      side: const BorderSide(color: Colors.black, width: 2.5),
+      onChanged: isChangedCallback,
     );
   }
 }
