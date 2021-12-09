75f5330398eab11f06ba7080a39eeea2e23a6616
Junaed <junaed1995@gmail.com>
2021-12-09 22:25:25 +0600

    BottomSheet added

--- lib/screen/todos_screen.dart
+++ lib/screen/todos_screen.dart
@@ -6,7 +6,49 @@ class TodosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            backgroundColor: const Color(0x00FFFFFF),
+            context: context,
+            builder: (context) => Container(
+              padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "Add Task",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(color: Colors.lightBlueAccent, fontSize: 30),
+                  ),
+                  const TextField(
+                    autofocus: true,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      isDense: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.lightBlueAccent,
+                    child: const Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
         backgroundColor: Colors.lightBlueAccent,
         child: const Icon(Icons.add),
       ),
